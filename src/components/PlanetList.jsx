7f1136e08c92eff6a2d4ab7d720924d455d94c8f
Jake McInerney <jakemcinerney1@gmail.com>
2022-06-23 17:32:03 -0500

    rendered all planets and all people onto the DOM

--- src/components/PlanetList.jsx
+++ src/components/PlanetList.jsx
@@ -13,13 +13,16 @@ function PlanetList() {
     dispatch({
       type: 'FETCH_PEOPLE',
     });
-  }, []);
+  }, [dispatch]);
+
+  console.log('planets****************************', planets);
 
   return (
     <div className='planets-container'>
       <ul>
         {planets &&
           planets.map((planet) => {
+            console.log('planet', planet);
             return (
               <li id={planet.name} className='individual-planet'>
                 {planet.name}
